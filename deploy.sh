948de85039416c296bcfc8ff4271bfb1869fa85e
Jose Monroy <mnry7999@gmail.com>
2023-02-25 12:41:05 -0600

    Copnfiguracion para depliegue

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/JoseLuisMonroy/Front-SaberesPrevios.git master:gh-pages
